439dfed94a3a4257b3b67f59fb216fccba446bf0
Johan González <johanfer_12@hotmail.com>
2023-07-13 19:09:08 -0500

    Interface upgrade
    Genre graph added

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from dash import Dash, dcc, html
 import sqlite3
-import os
 import ast
+import plotly.graph_objs as go
+import os
 
-# Obtiene la ruta absoluta al directorio raíz del código
+# Obtener la ruta absoluta al directorio raíz del código
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Conectar a la base de datos en el directorio raíz
@@ -18,6 +19,8 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS spotify_stats (
                     most_played_songs TEXT,
                     total_playtime INTEGER,
                     top_artists TEXT,
+                    top_genres TEXT,
+                    top_genres_percentages TEXT,
                     recent_favorite_songs TEXT
                 )''')
 
@@ -29,20 +32,36 @@ if row:
     most_played_songs = row[2]
     total_playtime = row[3]
     top_artists = row[4]
-    recent_favorite_songs = row[5]
+    top_genres = row[5]
+    top_genres_percentages = row[6]
+    recent_favorite_songs = row[7]
 else:
     most_played_artist = "N/A"
     most_played_songs = "N/A"
     total_playtime = 0
     top_artists = "N/A"
+    top_genres = "N/A"
+    top_genres_percentages = "N/A"
     recent_favorite_songs = "N/A"
 
 # Convertir las listas en bullet points con enlaces
 most_played_songs_list = ast.literal_eval(most_played_songs)
-most_played_songs_bullet = html.Ul([html.Li(html.A(f"{song['song_name']} - {song['artist_name']}", href=song['song_url'])) for song in most_played_songs_list])
+most_played_songs_bullet = html.P([
+    html.P([
+        html.I(className="fas fa-play", style={"margin-right": "5px", "margin-left": "12px"}),
+        html.A(f"{song['song_name']} - {song['artist_name']}", href=song['song_url'])
+    ])
+    for song in most_played_songs_list
+])
 
 recent_favorite_songs_list = ast.literal_eval(recent_favorite_songs)
-recent_favorite_songs_bullet = html.Ul([html.Li(html.A(f"{song['song_name']} - {song['artist_name']}", href=song['song_url'])) for song in recent_favorite_songs_list])
+recent_favorite_songs_bullet = html.P([
+    html.P([
+        html.I(className="fas fa-play", style={"margin-right": "5px", "margin-left": "12px"}),
+        html.A(f"{song['song_name']} - {song['artist_name']}", href=song['song_url'])
+    ])
+    for song in recent_favorite_songs_list
+])
 
 top_artists_list = top_artists.split(", ")
 top_artists_bullet = html.Ul([html.Li(artist) for artist in top_artists_list])
@@ -53,22 +72,51 @@ minutes = (total_playtime // 60) % 60
 hours = (total_playtime // 3600) % 24
 days = total_playtime // 86400
 
+# Convertir los géneros y porcentajes en datos para la gráfica de torta
+top_genres_list = ast.literal_eval(top_genres)
+top_genres_percentages_list = ast.literal_eval(top_genres_percentages)
+
+data = [go.Pie(labels=top_genres_list, values=top_genres_percentages_list)]
+
+# Incluir los estilos de Font Awesome
+external_stylesheets = [
+    "https://use.fontawesome.com/releases/v5.15.3/css/all.css"
+]
+
 # Crear una instancia de la aplicación Dash
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = 'Dash de Johan'
 
 # Definir el diseño de la aplicación
 app.layout = html.Div(
+    className='container',
     children=[
-        html.H1('Estadísticas del Spotify de Johan'),
-        html.P(f'Artista más escuchado: {most_played_artist}'),
-        html.P('Canciones más reproducidas:'),
-        most_played_songs_bullet,
-        html.P(f'Tiempo total de reproducción: {days} días, {hours} horas, {minutes} minutos, {seconds} segundos'),
-        html.P('Artistas más escuchados:'),
-        top_artists_bullet,
-        html.P('Canciones favoritas recientes:'),
-        recent_favorite_songs_bullet
+        html.Div(
+            className='left-column',
+            children=[
+                html.H1('Estadísticas Spotify de Johan'),
+                html.P(f'Artista más escuchado: {most_played_artist}'),
+                html.P('Canciones más reproducidas:'),
+                most_played_songs_bullet,
+                html.P(f'Tiempo total de reproducción: {days} días, {hours} horas, {minutes} minutos, {seconds} segundos'),
+                html.P('Artistas más escuchados:'),
+                top_artists_bullet,
+                html.P('Canciones favoritas recientes:'),
+                recent_favorite_songs_bullet,
+            ]
+        ),
+        html.Div(
+            className='right-column',
+            children=[
+                dcc.Graph(
+                    id='top-genres-chart',
+                    figure={
+                        'data': data,
+                        'layout': go.Layout(title='Géneros más escuchados', height=400, width=400, template='plotly_dark')
+                    }
+                )
+            ]
+        )
     ]
 )
 
--- refreshdb.py
+++ refreshdb.py
@@ -4,7 +4,7 @@ import sqlite3
 from config import CLIENT_ID, CLIENT_SECRET, USERNAME
 import os
 
-# Obtiene la ruta absoluta al directorio raíz del código
+# Obtener la ruta absoluta al directorio raíz del código
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Conectar a la base de datos en el directorio raíz
@@ -14,23 +14,25 @@ cursor = conn.cursor()
 
 # Crear una tabla para almacenar las estadísticas
 cursor.execute('''CREATE TABLE IF NOT EXISTS spotify_stats (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    most_played_artist TEXT,
-                    most_played_songs TEXT,
-                    total_playtime INTEGER,
-                    top_artists TEXT,
-                    recent_favorite_songs TEXT
-                )''')
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                most_played_artist TEXT,
+                most_played_songs TEXT, 
+                total_playtime INTEGER,
+                top_artists TEXT,
+                top_genres TEXT,
+                top_genres_percentages TEXT,
+                recent_favorite_songs TEXT
+            )''')
 
-# Obtén un token de acceso
+# Obtener el token de acceso
 scope = "user-top-read user-library-read"
 redirect_uri = "http://localhost:8888/callback"
 token = util.prompt_for_user_token(USERNAME, scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=redirect_uri)
 
-# Crea una instancia de Spotipy usando el token de acceso
+# Crear una instancia de Spotipy usando el token de acceso
 sp = spotipy.Spotify(auth=token)
 
-# Obtén el artista más escuchado
+# Obtener el artista más escuchado
 top_artists = sp.current_user_top_artists(limit=1, time_range='long_term')
 if top_artists['items']:
     most_played_artist = top_artists['items'][0]['name']
@@ -39,7 +41,7 @@ else:
     most_played_artist = "N/A"
     most_played_artist_image = None
 
-# Obtén las 5 canciones más reproducidas
+# Obtener las 5 canciones más reproducidas
 top_tracks = sp.current_user_top_tracks(limit=5, time_range='long_term')
 most_played_songs = []
 for track in top_tracks['items']:
@@ -48,36 +50,63 @@ for track in top_tracks['items']:
     song_url = track['external_urls']['spotify']
     most_played_songs.append({'song_name': song_name, 'artist_name': artist_name, 'song_url': song_url})
 
-# Obtén la duración total de reproducción
+# Obtener la duración total de reproducción
 playlists = sp.current_user_playlists()
 total_playtime = 0
 
-# Obtén las canciones guardadas como favoritos
+# Obtener las canciones guardadas como favoritos
 favorite_songs = sp.current_user_saved_tracks(limit=50)  # Aumenta el límite si es necesario
 
-# Suma la duración de cada canción
+# Sumar la duración de cada canción
 for track in favorite_songs['items']:
     duration_ms = track['track']['duration_ms']
     total_playtime += duration_ms
 
-# Obtén las listas de reproducción
+# Obtener las listas de reproducción
 playlists = sp.current_user_playlists()
 
-# Suma la duración de cada canción en las listas de reproducción
+# Sumar la duración de cada canción en las listas de reproducción
 for playlist in playlists['items']:
     tracks = sp.playlist_tracks(playlist['id'], fields='items(track(duration_ms))')
     total_playtime += sum(track['track']['duration_ms'] for track in tracks['items'])
 
 total_playtime = total_playtime // 1000  # Convertir de milisegundos a segundos
 
-print(total_playtime)
-
-
-# Obtén los 5 artistas más escuchados
+# Obtener los 5 artistas más escuchados
 top_artists = sp.current_user_top_artists(limit=5, time_range='long_term')
 top_artists_names = [artist['name'] for artist in top_artists['items']]
 
-# Obtén las 5 canciones más recientes añadidas a favoritos
+# Obtener los 5 géneros más escuchados con sus porcentajes
+top_genres = {}
+total_duration = 0
+
+for artist in top_artists['items']:
+    genres = artist['genres']
+    artist_tracks = sp.artist_top_tracks(artist['id'])['tracks']
+    num_tracks = min(len(artist_tracks), 10)  # Ajusta el número de canciones según sea necesario
+    duration_ms = sum(track['duration_ms'] for track in artist_tracks[:num_tracks])
+    total_duration += duration_ms
+
+    for genre in genres:
+        if genre in top_genres:
+            top_genres[genre] += duration_ms
+        else:
+            top_genres[genre] = duration_ms
+
+# Calcula los porcentajes de tiempo dedicados a cada género
+genre_percentages = {}
+for genre, duration in top_genres.items():
+    percentage = (duration / total_duration) * 100
+    genre_percentages[genre] = percentage
+
+# Ordena los géneros por porcentaje en orden descendente
+sorted_genres = sorted(genre_percentages.items(), key=lambda x: x[1], reverse=True)
+
+# Obtén los 5 géneros más escuchados y sus porcentajes
+top_genres_names = [genre for genre, _ in sorted_genres[:5]]
+top_genres_percentages = [percentage for _, percentage in sorted_genres[:5]]
+
+# Obtener las 5 canciones más recientes añadidas a favoritos
 favorite_songs = sp.current_user_saved_tracks(limit=5)
 recent_favorite_songs = []
 for track in favorite_songs['items']:
@@ -88,8 +117,8 @@ for track in favorite_songs['items']:
 
 # Inserta las estadísticas en la base de datos
 cursor.execute('DELETE FROM spotify_stats')
-cursor.execute('INSERT INTO spotify_stats (most_played_artist, most_played_songs, total_playtime, top_artists, recent_favorite_songs) VALUES (?, ?, ?, ?, ?)',
-               (most_played_artist, str(most_played_songs), total_playtime, ", ".join(top_artists_names), str(recent_favorite_songs)))
+cursor.execute('INSERT INTO spotify_stats (most_played_artist, most_played_songs, total_playtime, top_artists, top_genres, top_genres_percentages, recent_favorite_songs) VALUES (?, ?, ?, ?, ?, ?, ?)',
+               (most_played_artist, str(most_played_songs), total_playtime, ", ".join(top_artists_names), str(top_genres_names), str(top_genres_percentages), str(recent_favorite_songs)))
 
 # Guarda los cambios y cierra la conexión con la base de datos
 conn.commit()
